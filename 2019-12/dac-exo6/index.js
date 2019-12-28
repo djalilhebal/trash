d2351fcd67dad960c95bc475bc5d1b754ab392b8
Djalil Dreamski <dreamski21@gmail.com>
2019-12-28 14:55:08 +0100

    Rename classes, methods, and variables

--- 2019-12/dac-exo6/index.js
+++ 2019-12/dac-exo6/index.js
@@ -16,7 +16,7 @@ function zipObject(arrA, arrB) {
  * A Promise-based and queue-based Semaphore
  * @todo maybe fork or learn from https://github.com/zeit/async-sema
  */
-const Sema = class SillySemaphore {
+const Semaphore = class SillySemaphore {
 
   constructor(permits) {
     this._permits = permits;
@@ -56,38 +56,39 @@ const Sema = class SillySemaphore {
 
 }
 
-class Exo6 {
+class Sim {
 
   static userVars = {};
   static userAlgos = {};
+
+  static chan  = null;
   static voie1 = [];
   static voie2 = [];
-  static chan = null;
 
   static ui = {};
   static creatorInterval = null;
   static drawerInterval = null;
 
   static start() {
-    Exo6.clearErrors();
-    Exo6.loadUserInputs();
-    Exo6.ui.$fieldset.disabled = true;
-    Exo6.initCreator();
-    Exo6.initDrawer();
+    Sim.clearErrors();
+    Sim.loadUserInputs();
+    Sim.ui.$fieldset.disabled = true;
+    Sim.initCreator();
+    Sim.initDrawer();
     // ...
   }
 
   static stop() {
-    clearInterval(Exo6.creatorInterval);
-    clearInterval(Exo6.drawerInterval);
-    Exo6.ui.$fieldset.disabled = false;
+    clearInterval(Sim.creatorInterval);
+    clearInterval(Sim.drawerInterval);
+    Sim.ui.$fieldset.disabled = false;
     // ...
   }
 
   static setup() {
     const $ = str => document.querySelector(str);
 
-    Object.assign(Exo6.ui, {
+    Object.assign(Sim.ui, {
       $form: $('form'),
       $fieldset: $('form fieldset'),
       $changement: $('#changement'),
@@ -102,17 +103,17 @@ class Exo6 {
       $carrefour: $('#carrefour'),
     })
 
-    Exo6.ui.$form.onsubmit = (ev) => {
+    Sim.ui.$form.onsubmit = (ev) => {
       ev.preventDefault();
-      Exo6.start();
+      Sim.start();
     }
   }
   
   static loadUserInputs() {
-    const {ui} = Exo6;
+    const {ui} = Sim;
 
     // userAlgos...
-    Object.assign(Exo6.userAlgos,
+    Object.assign(Sim.userAlgos,
       {
         changement: ui.$changement.value,
         traversee1: ui.$traversee1.value,
@@ -121,66 +122,78 @@ class Exo6 {
     )
 
     // userVars...
-    const formData = new FormData(ui.$form);
+    const fd = new FormData(ui.$form);
     const SEP = /,\s*/; // separator
 
     const userSemas = zipObject(
-      formData.get('sema-names').split(SEP),
-      formData.get('sema-vals').split(SEP).map(val => new Sema(Number(val)))
+      fd.get('sema-names').split(SEP),
+      fd.get('sema-vals').split(SEP).map(val => new Semaphore(Number(val)))
     );
 
     const userInts = zipObject(
-      formData.get('int-names').split(SEP),
-      formData.get('int-vals').split(SEP).map(val => Number(val))
+      fd.get('int-names').split(SEP),
+      fd.get('int-vals').split(SEP).map(val => Number(val))
     );
 
-    Object.assign(Exo6.userVars, userSemas, userInts);
+    Object.assign(Sim.userVars, userSemas, userInts);
   }
 
   static initCreator() {
-    Exo6.chan = new Changement();
-
-    // Maybe create a new instance of Traversee every 2 secs
-    Exo6.creatorInterval = setInterval(function maybeCreate() {
-      if (Math.random() < 0.5) return;
-      const {voie1, voie2} = Exo6;
-      if (Math.random() < 0.5) {
-        if (voie1.length < Traversee.MAX_PAR_VOIE) {
-          voie1.push( new Traversee1() );
-        }
-      } else {
-        if (voie2.length < Traversee.MAX_PAR_VOIE) {
-          voie2.push( new Traversee2() );
-        }
-      }
-    }, 2 * 1000);
+    Sim.chan = new Changement();
+    Sim.creatorInterval = setInterval(Sim.maybeCreateTraversee, 2 * 1000);
   }
 
   static initDrawer() {
     // HACK: Redraw every 1/4 sec
-    Exo6.drawerInterval = setInterval(Exo6.redraw, 0.25 * 1000);
+    Sim.drawerInterval = setInterval(Sim.redraw, 0.25 * 1000);
+  }
+
+  /**
+   * Maybe create a new instance of Traversee every 2 secs
+   * @todo Make voieX.push(..) part of Traversee's own logic
+   */
+  static maybeCreateTraversee() {
+    if (Math.random() < 0.5) return;
+    const {voie1, voie2} = Sim;
+    if (Math.random() < 0.5) {
+      if (voie1.length < Traversee.MAX_PAR_VOIE) {
+        voie1.push( new Traversee1() );
+      }
+    } else {
+      if (voie2.length < Traversee.MAX_PAR_VOIE) {
+        voie2.push( new Traversee2() );
+      }
+    }
   }
 
   static redraw() {
     // Just update dataset and order values, and let CSS take care of the rest.
 
     // Feu
-    Exo6.ui.$sim.dataset.feu = Exo6.userVars.feu;
+    Sim.ui.$sim.dataset.feu = Sim.userVars.feu;
 
     // Voies
-    const {voie1, voie2} = Exo6;
+    const {voie1, voie2} = Sim;
     const tousTraversees = [...voie1, ...voie2];
     tousTraversees.sort(Traversee.makeSorter());
-    tousTraversees.forEach((t, i) => t.$elem.style.order = i);
+    tousTraversees.forEach((t, i) => {
+      t.$elem.title =
+`${t.color} ${t.type} #${t.id}
+
+orderVec: {${t.orderVec.join(', ')}}
+waitVec: {${t.getWaitVec().join(', ')}}`
+      t.$elem.style.order = i;
+    });
   }
 
   static showError(algoSource, message) {
-    Exo6.ui[ '$' + algoSource ].setAttribute('title', message);
-    Exo6.stop();
+    Sim.ui[ '$' + algoSource ].setAttribute('title', message);
+    Sim.stop();
+    // Sim.freezeUI(); // maybe?
   }
 
   static clearErrors() {
-    Exo6.ui.$form.querySelectorAll('[title]').forEach($x => $x.removeAttribute('title'));
+    Sim.ui.$form.querySelectorAll('[title]').forEach($x => $x.removeAttribute('title'));
   }
 
 }
@@ -192,7 +205,7 @@ class Algorithme {
    */
   constructor(algoSource) {
     this.algoSource = algoSource;
-    this.userAlgo   = Exo6.userAlgos[algoSource];
+    this.userAlgo   = Sim.userAlgos[algoSource];
     this.orderVec   = Algorithme.parseOrderVector(this.userAlgo);
   }
 
@@ -201,14 +214,14 @@ class Algorithme {
     try {
       await new AsyncFunction('that', `
       with (that) {
-        with (Exo6.userVars) {
+        with (Sim.userVars) {
           ${ Algorithme.awaitify(this.userAlgo) }
         }
       }
       `)(this);
     } catch (userError) {
       console.error('userError', userError);
-      Exo6.showError(this.algoSource, userError.message);
+      Sim.showError(this.algoSource, userError.message);
     }
   }
 
@@ -279,7 +292,7 @@ class Traversee extends Algorithme {
     this.type = Math.random() < 0.25 ? 'truck' : 'car'; // 25% chance of being a truck
 
     // TODO Move to dedicated method?
-    // TODO Stop replying on .traversee1 and .traversee2 (.<algoSource>)
+    // TODO Stop relying on .traversee1 and .traversee2 (.<algoSource>)
     this.$elem = document.createElement('span');
     this.$elem.classList.add('vehicle', this.type, this.algoSource);
     this.$elem.title = `${this.color} ${this.type} #${this.id}`;
@@ -297,26 +310,25 @@ class Traversee extends Algorithme {
   }
 
   async enterIntersection() {
-    Exo6.ui.$carrefour.append(
+    Sim.ui.$carrefour.append(
       this.$elem.parentNode.removeChild(this.$elem)
     )
-    this.checkCollision();
+    this.assertNoCollision();
     await this.sleep(1);
   }
 
   async leaveIntersection() {
     this.$elem.dataset.state = 'leaving';
     await this.sleep(1);
-    this.checkCollision();
+    this.assertNoCollision();
     this.destroy();
   }
 
-  checkCollision() {
-    const $c = Exo6.ui.$carrefour;
-    const happened = $c.children.length > 1;
+  assertNoCollision() {
+    const $c = Sim.ui.$carrefour;
+    const happened = $c.children.length > 1; // more than one vehicle crossing the intersection
     if (happened) {
-      // state = enum {'normal', 'collision'}
-      $c.dataset.state = 'collision';
+      $c.dataset.state = 'collision'; // enum {'normal', 'collision'}
       throw new Error('Collision!');
     }
   }
@@ -325,7 +337,7 @@ class Traversee extends Algorithme {
    * @returns {Array<number>}
    */
   getWaitVec() {
-    const vec = this.orderVec.map(semaName => Exo6.userVars[semaName].getPosition(this));
+    const vec = this.orderVec.map(semaName => Sim.userVars[semaName].getPosition(this));
     return vec;
   }
 
@@ -344,9 +356,9 @@ class Traversee extends Algorithme {
     // TODO Move to child classes
     // remove from the road's list
     if (this.algoSource === 'traversee1') {
-      Exo6.voie1.splice(Exo6.voie1.findIndex(t => t === this), 1);
+      Sim.voie1.splice(Sim.voie1.findIndex(t => t === this), 1);
     } else {
-      Exo6.voie2.splice(Exo6.voie2.findIndex(t => t === this), 1);
+      Sim.voie2.splice(Sim.voie2.findIndex(t => t === this), 1);
     }
   }
 
@@ -397,7 +409,7 @@ class Traversee1 extends Traversee {
 
   constructor() {
     super('traversee1');
-    Exo6.ui.$voie1.append(this.$elem);
+    Sim.ui.$voie1.append(this.$elem);
     this.run();
   }
 
@@ -408,10 +420,10 @@ class Traversee2 extends Traversee {
 
   constructor() {
     super('traversee2');
-    Exo6.ui.$voie2.append(this.$elem);
+    Sim.ui.$voie2.append(this.$elem);
     this.run();
   }
 
 }
 
-Exo6.setup();
+Sim.setup();
